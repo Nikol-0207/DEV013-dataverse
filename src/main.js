f71a77f86a4d6c14678b17c324388942609aa227
Nikol-0207 <pwendynicol@gmail.com>
2024-01-18 13:43:32 -0300

    Prueba de Git

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
-
+//prueba2
 console.log(example, renderItems(data), data);

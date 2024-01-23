a2b7e0b97b7241c7f2c84818b9b29e4081678be2
karenmora02 <morak970@gmail.com>
2024-01-23 14:47:15 -0500

    Añade informacion a las tarjetas

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
-<<<<<<< HEAD
+
 
 console.log("aqui main"+ example, renderItems(data), data);
 
@@ -61,7 +61,6 @@ searchButton.addEventListener('click', () => {
   }
 
 });
-=======
-//prueba2
-console.log(example, renderItems(data), data);
->>>>>>> 99d95335d15b7ed1d15d828576548147095ed310
+
+
+

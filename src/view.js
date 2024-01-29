a15f5ab5e3c0ca24ec56ee9715792a5e2f5c5b32
Nikol-0207 <pwendynicol@gmail.com>
2024-01-29 11:52:13 -0300

    Datos de la descripcion que iran al reverso de las tarjetas (view.js)

--- src/view.js
+++ src/view.js
@@ -5,41 +5,96 @@
  * @author wendy nicol
  */
 export const renderItems = (data) => {
+
+
+
+/*
+BEM
+block
+element
+modificador
+
+https://css-tricks.com/bem-101/
+
+
+<li>
+  <section class="card-front">
+    <header class="card-front__header">
+      <figure class="card-front__header-image">
+        <img src alt />
+      </figure>
+      <h2></h2>
+    </header>
+    <footer class="card-front__footer">
+      <figure class="card-front__footer-image">
+        <img src alt />
+      </figure>
+    </footer>
+  </section>
+  <section class="card-back">
+    <header class="card-back__header">
+      <h2 data-itemprop="name" ></h2>
+    </header>
+    <main class="card-back__content">
+      <dl itemscope itemtype="consoles" class="card-back__details-list">
+        <dt class="card-back__details-title">Descripción:</dt><dd >Pionera de la informática, fue la primera programadora.</dd>
+        <dt class="card-back__details-title">Tipo de consola:</dt><dd itemprop="tipo">1843</dd>
+        <dt>Precio de lanzamiento:</dt><dd itemprop="PrecioDeLanzamiento">London, England</dd>
+        <dt>Duración en el mercado:</dt><dd itemprop="DuraciónEnElMercado">Computer Science</dd>
+        <dt>Generacion:</dt><dd itemprop="generation">Computer Science</dd>
+      </dl>
+    </main>
+    <footer class="card-back__footer">
+      <figure class="card-back__footer-image">
+        <img src alt data-itemprop="imageUrl" />
+        <span data-itemprop="JuegoMásPopular" >Super Mario 64</span>
+      </figure>
+    </footer>
+  </section>
+</li>
+*/
   const $contenedorTarjetas = document.createElement("ul");
   $contenedorTarjetas.setAttribute("class", "ul");
 
   data.forEach((item) => {
     const $tarjeta = document.createElement("li");
 
-    // <img src=item.imageUrl alt=item.name class=img />
-    const $imagen = document.createElement("img");
-    $imagen.setAttribute("src", item.imageUrl);
-    $imagen.setAttribute("alt", item.name);
-    $imagen.setAttribute("class", "img");
+    $tarjeta.innerHTML=`
+      <section class="card-front">
+        <header class="card-front__header">
+          <figure class="card-front__header-image">
+            <img src="${item.imageUrl}" alt  data-itemprop="imageUrl"  />
+          </figure>
+          <h2></h2>
+        </header>
+        <footer class="card-front__footer">
+          <figure class="card-front__footer-image">
+            <img src alt />
+          </figure>
+        </footer>
+      </section>
+      <section class="card-back">
+        <header class="card-back__header">
+          <h2 data-itemprop="name" ></h2>
+        </header>
+        <main class="card-back__content">
+          <dl itemscope itemtype="consoles" class="card-back__details-list">
+            <dt class="card-back__details-title">Descripción:</dt><dd  itemprop="description">${item.description}</dd>
+            <dt class="card-back__details-title">Tipo de consola:</dt><dd itemprop="tipo">${item.tipo}</dd>
+            <dt class="card-back__details-title">Precio de lanzamiento:</dt><dd itemprop="PrecioDeLanzamiento">${item.PrecioDeLanzamiento}</dd>
+            <dt class="card-back__details-title">Duración en el mercado:</dt><dd itemprop="DuraciónEnElMercado">${item.DuraciónEnElMercado}</dd>
+            <dt class="card-back__details-title">Generacion:</dt><dd itemprop="generation">${item.generation}</dd>
+          </dl>
+        </main>
+        <footer class="card-back__footer">
+          <figure class="card-back__footer-image">
+            <img src alt  data-itemprop="imageUrlGame" />
+            <span data-itemprop="JuegoMásPopular" >${item.JuegoMásPopular}</span>
+          </figure>
+        </footer>
+      </section>
 
-    // <p class=parrafo>item.description</p>
-    const $parrafo = document.createElement("p");
-    $parrafo.setAttribute("class", "parrafo");
-    //$parrafo.innerText = item.description;
-
-    //creacion de div
-    const $contenedordiv = document.createElement("div");
-    $contenedordiv.setAttribute("class","contenedor-div");
-    // <h2>item.name</h2>
-    const $titulo = document.createElement("h2");
-    $titulo.innerText = item.name;
-
-    /*
-     <li>
-      <img src=item.imageUrl alt=item.name class=img />
-      <h2>item.name</h2>
-      <p class=parrafo>item.description</p>
-     </li>
-     */
-    $tarjeta.appendChild($imagen);
-    $tarjeta.appendChild($titulo);
-    $tarjeta.appendChild($parrafo);
-    $tarjeta.appendChild($contenedordiv);
+    `;
 
     $contenedorTarjetas.appendChild($tarjeta);
   });

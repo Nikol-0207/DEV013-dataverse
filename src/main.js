5f20387cc620ef7937a78720bc1a5a2b2f2f33a9
karenmora02 <morak970@gmail.com>
2024-02-10 22:58:25 -0500

    agrego datos a la estadistica con respecto al precio y generacion promediando el mas alto por generacion

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import { orderType, orderGeneration } from "./dataFunctions.js";
 import { renderItems, renderBackground } from "./view.js";
 
-
 import data from "./data/dataset.js";
 
 //console.log("aqui main" + example, renderItems(data), data);
@@ -16,14 +15,12 @@ const $botonBuscar = document.querySelector("#searchName");
 const $contenedor = document.querySelector("#root");
 
 const popup = document.getElementById("popup");
-const blurBackground = document.querySelector('#blurBackground');
+const blurBackground = document.querySelector("#blurBackground");
 const closeButton = document.getElementById("closePopup");
 
-
-const statistics = document.querySelector('#statistics-database');
-const closeBox = document.querySelector('.closeHistogram');
-const buttonStatistics = document.querySelector('#statics');
-
+const statistics = document.querySelector("#statistics-database");
+const closeBox = document.querySelector(".closeHistogram");
+const buttonStatistics = document.querySelector("#statics");
 
 const actualizarTarjetas = (data) => {
   const $contenedorTarjetas = document.querySelector(".container-item");
@@ -31,95 +28,126 @@ const actualizarTarjetas = (data) => {
   $contenedor.appendChild(renderItems(data));
 };
 
-$iconos.appendChild(renderBackground())
+$iconos.appendChild(renderBackground());
 actualizarTarjetas(data);
 
-// Filtrar por generación
 $selection.addEventListener("input", (event) => {
   const selectedGeneration = event.target.value;
   const filteredData = orderGeneration(selectedGeneration);
 
   actualizarTarjetas(filteredData);
 });
-// Filtrar por tipo
 $selecType.addEventListener("input", (event) => {
   const selectedType = event.target.value;
   const filteredDataByType = orderType(selectedType);
 
   actualizarTarjetas(filteredDataByType);
 });
-//ventanas emergente
 
-// Función para mostrar la ventana emergente
 function showPopup() {
-  blurBackground.style.display = 'block';
-  document.body.style.overflow='hidden';
+  blurBackground.style.display = "block";
+  document.body.style.overflow = "hidden";
   popup.style.display = "block";
 }
 
-// Función para ocultar la ventana emergente
 function hidePopup() {
-  blurBackground.style.display = 'none';
-  document.body.style.overflow = 'auto';
+  blurBackground.style.display = "none";
+  document.body.style.overflow = "auto";
   popup.style.display = "none";
 }
 
-// Asignar evento de click al botón de cerrar
 closeButton.addEventListener("click", hidePopup);
-//Ventana emergente de estadistica
 
-function showStatistics(){
-  blurBackground.style.display = 'block';
+function calcularSumaGeneraciones(arreglo) {
+  const sumaPorGeneracion = {};
+
+  arreglo.forEach((item) => {
+    const { PrecioDeLanzamiento, generation } = item;
+
+    const generacionNormalizada = generation.toLowerCase().replace(/\s+/g, "");
+
+    if (sumaPorGeneracion[generacionNormalizada]) {
+      sumaPorGeneracion[generacionNormalizada] += PrecioDeLanzamiento;
+    } else {
+      sumaPorGeneracion[generacionNormalizada] = PrecioDeLanzamiento;
+    }
+  });
+
+  return sumaPorGeneracion;
+}
+
+function getInfoToFilters() {
+  const cardsInformation = Array.from(document.querySelectorAll(`.card-back`));
+  console.log(cardsInformation);
+  const data = cardsInformation.map(function (card) {
+    return {
+      PrecioDeLanzamiento: parseInt(
+        card
+          .querySelector(`[itemprop="PrecioDeLanzamiento"]`)
+          .textContent.split(".")[0]
+          .replace(/\D/g, "")
+      ),
+      generation: card.querySelector(`[itemprop="generation"]`).textContent,
+    };
+  });
+  console.log(data);
+  const sumaGeneraciones = calcularSumaGeneraciones(data);
+  console.log(sumaGeneraciones);
+  return sumaGeneraciones;
+}
+let histograma;
+function showStatistics() {
+  blurBackground.style.display = "block";
   /* document.body.style.overflow='hidden'; */
   statistics.style.display = "block";
-  const ctx = document.getElementById('histograma').getContext('2d');
-
-  const histograma = new Chart(ctx, {
-    type: 'bar',
+  const ctx = document.getElementById("histograma").getContext("2d");
+  const generationPrice = getInfoToFilters();
+  histograma = new Chart(ctx, {
+    type: "polarArea",
     data: {
-      labels: ['Gen #1:Color Tv Game', 'Gen #2:Color Tv Game', 'Etiqueta 3', 'Etiqueta 4', 'Etiqueta 5', 'Etiqueta 6','Etiqueta'],
-      datasets: [{
-        label: 'Mayor Precio de Consola de cada Generación',
-        data: [199, 40, 179.99, 5, 2,50,100],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)'
-        ],
-        borderWidth: 1
-      }]
+      labels: Object.keys(generationPrice),
+      datasets: [
+        {
+          label: "Mayor Precio de Consola de cada Generación",
+          data: Object.values(generationPrice),
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       scales: {
         y: {
-          beginAtZero: true
-        }
-      }
-    }
+          beginAtZero: true,
+        },
+      },
+    },
   });
-
 }
 
-function hideStatistics(){
-  blurBackground.style.display = 'none';
+function hideStatistics() {
+  histograma.destroy();
+  blurBackground.style.display = "none";
   /* document.body.style.overflow='auto'; */
   statistics.style.display = "none";
 }
-buttonStatistics.addEventListener("click",showStatistics);
-closeBox.addEventListener("click",hideStatistics);
-
+buttonStatistics.addEventListener("click", showStatistics);
+closeBox.addEventListener("click", hideStatistics);
 
 // Crear el histograma con Chart.js
-
 
 // Filtrar por nombre
 
@@ -129,7 +157,10 @@ $botonBuscar.addEventListener("click", () => {
 
   // Filtra los elementos que contienen el término de búsqueda en el nombre
   const filteredData = data.filter((item) =>
-    item.name.replaceAll(" ", "").toLowerCase().includes(searchTerm.replaceAll(" ", "").toLowerCase())
+    item.name
+      .replaceAll(" ", "")
+      .toLowerCase()
+      .includes(searchTerm.replaceAll(" ", "").toLowerCase())
   );
 
   if (filteredData.length > 0) {
